267eca5a639f11edb4c139967d30704a7ec776b0
jannatulnila <jannatulnila74@gmail.com>
2026-01-18 13:24:58 +0600

    implement user authorization and email verification for post creation

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -3,7 +3,14 @@ import { postService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
   try {
-    const result = await postService.createPost(req.body);
+    const user = req.user;
+    if(!req.user){
+      return res.status(401).json({
+        success:false,
+        message:"You are not authorized"
+      })
+    }
+    const result = await postService.createPost(req.body, req.user.id as string);
 
     return res.status(201).json({
       success: true,
@@ -14,6 +21,7 @@ const createPost = async (req: Request, res: Response) => {
     return res.status(400).json({
       success: false,
       error: "Failed to create post",
+      details: error
     });
   }
 };
--- src/modules/post/post.router.ts
+++ src/modules/post/post.router.ts
@@ -1,8 +1,62 @@
-import express, { Router }  from "express";
+import express, { NextFunction, Request, Response, Router }  from "express";
 import { postController } from "./post.controller";
+import {auth as betterAuth} from '../../lib/auth';
+import { success } from "better-auth/*";
+import { UserRole } from "../../../generated/prisma/enums";
 
 const router = express.Router();
 
-router.post("/", postController.createPost)
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: {
+                id:string;
+                email:string;
+                name:string;
+                role:string;
+                emailVerified:boolean;
+            }
+    }
+}
+}
+const auth =(...roles:UserRole[])=>{
+    return async (req:Request, res:Response, next:NextFunction)=>{
+        // get user session
+        const session = await betterAuth.api.getSession({
+            headers: req.headers as any
+        });
+        if(!session){
+            return res.status(401).json({
+                success:false,
+                message:"You are not authorized"})
+        } 
+        if(!session.user.emailVerified){
+            return res.status(401).json({
+                success:false,
+                message:"Email verification required. Please verify your email address"})
+        }
+        req.user = {
+            id:session.user.id,
+            email:session.user.email,
+            name:session.user.name,
+            role:session.user.role as string,
+            emailVerified:session.user.emailVerified,
+        }
+        if(roles.length && !roles.includes(session.user.role as UserRole)){
+            return res.status(403).json({
+                success:false,
+                message:"You do not have permission to access this resource"
+            })
+        }
+        next()
+
+    }
+    
+}
+
+router.post("/",
+     auth(UserRole.USER),
+      postController.createPost)
 
 export const postRouter: Router = router;
