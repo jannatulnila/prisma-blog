3d36470912b0ff3cd48d9ef64cc2f5d5f05948f0
jannatulnila <jannatulnila74@gmail.com>
2026-01-19 19:48:08 +0600

    update getAllPost function to use destructured parameters and improve search logic

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,5 @@
 import { Post } from "../../../generated/prisma/client";
+import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
 const createPost = async(data: Omit<Post, 'id'|'createdAt'|'updatedAt' | "authorId">, userId: string)=>{
@@ -11,42 +12,53 @@ const createPost = async(data: Omit<Post, 'id'|'createdAt'|'updatedAt' | "author
     return result;
     
 }
-const getAllPost = async(payload: {
-    search: string | undefined
+const getAllPost = async({search,tags}: {
+    search: string | undefined,
     tags: string[] | []
 })=>{
-    const allPost = await prisma.post.findMany({
-        where:{
-           AND: [
-             {
+    const andConditions: PostWhereInput[]= [];
+    if(search){
+        andConditions.push({
                 OR:[
                 {
                     title:{
-                contains: payload.search as string,
+                contains: search, 
                 mode:"insensitive",
                 }
 
             },
             {
                 content:{
-                contains: payload.search as string,
+                contains: search ,
                 mode:"insensitive",
 
             }
             },
             {
                 tags: {
-                has: payload.search as string,
+                has: search 
                 }
             }
             ],
-             },
-             {
-             tags:{
-                hasEvery: payload.tags as string[]
             }
+        )
+    }
+
+   
+    if(tags.length > 0){
+        andConditions.push(
+             {
+                 tags:{
+                hasEvery: tags as string[]
+            
            }
-           ]
+            }
+        )
+    }
+
+    const allPost = await prisma.post.findMany({
+        where:{
+           AND: andConditions
         }
     });
     return allPost;
