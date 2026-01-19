9588a7e7d05074823bbfe1649645413d1f0a155f
jannatulnila <jannatulnila74@gmail.com>
2026-01-19 20:59:52 +0600

    update Post model status default to PUBLISHED and enhance getAllPost function with status and authorId filtering

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
 
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -30,11 +31,21 @@ const getAllPost= async(req:Request, res:Response)=>{
   try {
     const {search} = req.query;
     const searchString = typeof search === "string" ? search : undefined;
-    const tags = req.query.tags ? (req.query.tags as string).split(",") :[]
-    const isFeatured = req.query.isFeatured ? req.query.isFeatured === 'true' : false;
+    const tags = req.query.tags ? (req.query.tags as string).split(",") :[];
+
+    const isFeatured = req.query.isFeatured 
+           ? req.query.isFeatured === 'true' 
+           ? true 
+           : req.query.isFeatured === 'false' 
+              ? false 
+              : undefined
+           : undefined;
+      
+    const status = req.query.status as PostStatus | undefined;       
+    const authorId = req.query.authorId as string | undefined;
     console.log({isFeatured});
 
-    const result = await postService.getAllPost({search: searchString, tags, isFeatured});
+    const result = await postService.getAllPost({search: searchString, tags, isFeatured, status, authorId});
     res.status(200).json(result)
   } catch (error) {
      res.status(400).json({
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,4 @@
-import { Post } from "../../../generated/prisma/client";
+import { Post, PostStatus } from "../../../generated/prisma/client";
 import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
@@ -13,10 +13,17 @@ const createPost = async(data: Omit<Post, 'id'|'createdAt'|'updatedAt' | "author
     
 }
 const getAllPost = async({
-    search,tags, isFeatured}: {
+    search,
+    tags, 
+    isFeatured,
+    status,
+    authorId
+}: {
     search: string | undefined,
     tags: string[] | []
-    isFeatured?: boolean
+    isFeatured: boolean | undefined
+    status: PostStatus | undefined
+    authorId: string | undefined
 })=>{
     const andConditions: PostWhereInput[]= [];
     if(search){
@@ -62,6 +69,16 @@ const getAllPost = async({
         andConditions.push({
             isFeatured
         })
+    }
+
+    if(status){
+        andConditions.push({
+            status
+        })
+    }
+
+    if(authorId){
+        authorId
     }
 
     const allPost = await prisma.post.findMany({
