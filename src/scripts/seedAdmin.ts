d8d0d367a91c594ab2355637708ea2c8f318bfa0
jannatulnila <jannatulnila74@gmail.com>
2026-01-20 21:20:18 +0600

    enhance seedAdmin script to use environment variables and improve admin user creation logic

--- src/scripts/seedAdmin.ts
+++ src/scripts/seedAdmin.ts
@@ -1,77 +1,90 @@
-// import { UserRole } from "../../generated/prisma/enums";
-// import { prisma } from "../lib/prisma";
-
 import { UserRole } from "../../generated/prisma/enums";
 import { prisma } from "../lib/prisma";
 
-// async function seedAdmin() {
-//     try {
-//         const adminData = {
-//             name: "admin",
-//             email: "admin@gmail.com",
-//             role: UserRole.ADMIN,
-//             password: "Admin@12345",
-//             emailVerified: true
-// };
-//         // check user exist on db or not
-//         const existingUser = await prisma.user.findUnique({
-//             where: {
-//                 email: adminData.email 
-//             }
-//         })
-
-//         if(existingUser){
-//             throw new Error("User already exists");
-//         }
-
-//         const signUpAdmin = await fetch('http://localhost:3000/api/auth/sign-up/email', {
-//             method: 'POST',
-//             headers: {  
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(adminData)
-//         });
-
-//        console.log(signUpAdmin);
-
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// seedAdmin();
-
-
-async function seedAdmin(){
+async function seedAdmin() {
     try {
-
-        const adminData ={
+        console.log("*** admin seeding started....",);
+        const adminData = {
             name: process.env.ADMIN_NAME!,
             email: process.env.ADMIN_EMAIL!,
             password: process.env.ADMIN_PASSWORD!,
             role: UserRole.ADMIN,
             emailVerified: true
-        }
-       //check your exist on db or not
-       const existingUser = await prisma.user.findUnique({
-        where: {
-            email: adminData.email
-        }
-       }) 
-         if(existingUser){
+        };
+         console.log("***chaking admin exist or not ....",);
+        // check user exist on db or not
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: adminData.email 
+            }
+        })
+
+        
+        if(existingUser){
             throw new Error("User already exists");
-         }
-            const signUpAdmin = await fetch('http://localhost:3000/api/auth/sign-up/email', {
+        }
+
+        const signUpAdmin = await fetch('http://localhost:3000/api/auth/sign-up/email', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
+            headers: {  
+                'Content-Type': 'application/json',
+                "origin": "http://localhost:3000"
             },
             body: JSON.stringify(adminData)
         });
-        console.log(signUpAdmin);
+        
+
+       if(signUpAdmin.ok){
+        console.log("**admin created");
+        await prisma.user.update({
+            where:{
+                email: adminData.email
+            },
+            data:{
+                emailVerified: true
+            }
+        })
+        console.log("***email verification  status update....");
+       }
+       console.log("***admin created successfully....",);
+
     } catch (error) {
-      console.log(error);  
+        console.log(error);
     }
 }
 
 seedAdmin();
+
+// async function seedAdmin(){
+//     try {
+
+//         const adminData ={
+//             name: process.env.ADMIN_NAME!,
+//             email: process.env.ADMIN_EMAIL!,
+//             password: process.env.ADMIN_PASSWORD!,
+//             role: UserRole.ADMIN,
+//             emailVerified: true
+//         }
+//        //check your exist on db or not
+//        const existingUser = await prisma.user.findUnique({
+//         where: {
+//             email: adminData.email
+//         }
+//        }) 
+//          if(existingUser){
+//             throw new Error("User already exists");
+//          }
+//             const signUpAdmin = await fetch('http://localhost:3000/api/auth/sign-up/email', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(adminData)
+//         });
+//         console.log(signUpAdmin);
+//     } catch (error) {
+//       console.log(error);  
+//     }
+// }
+
+// seedAdmin();
